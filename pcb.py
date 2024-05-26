5ec07f1feffac4bb66753f049b0e161a4500ffba
Tim Magoun <timmagoun@gmail.com>
2024-05-26 16:18:46 -0700

    lint

--- pcb.py
+++ pcb.py
@@ -1,18 +1,18 @@
 # from ast import Param
-import math
-import sys
 
-from solid import *
-from solid.utils import *
+from solid import (
+    union,
+    cube,
+)
+
+from solid.utils import right, forward, down
 
 import logging
 
-from cell import Cell
-from support import Support
 from parameters import Parameters
 
 
-class PCB():
+class PCB:
 
     def __init__(self, parameters: Parameters = Parameters()):
 
@@ -24,8 +24,12 @@ class PCB():
         self.pcb_width = self.parameters.pcb_width
         self.pcb_height = self.parameters.pcb_height
         self.pcb_top_left_coordinates = self.parameters.pcb_top_left_coordinates
-        self.pcb_left_switch_center_x_coordinate = self.parameters.pcb_left_switch_center_x_coordinate
-        self.pcb_top_switch_center_y_coordinate = self.parameters.pcb_top_switch_center_y_coordinate
+        self.pcb_left_switch_center_x_coordinate = (
+            self.parameters.pcb_left_switch_center_x_coordinate
+        )
+        self.pcb_top_switch_center_y_coordinate = (
+            self.parameters.pcb_top_switch_center_y_coordinate
+        )
         self.pcb_case_top_margin = self.parameters.pcb_case_top_margin
         self.pcb_case_bottom_margin = self.parameters.pcb_case_bottom_margin
         self.pcb_case_right_margin = self.parameters.pcb_case_right_margin
@@ -34,24 +38,26 @@ class PCB():
         self.pcb_thickness = 1.6
 
         self.pcb_plate_separation = 5
-    
 
-    def get_model(self, remove_above = False, remove_bellow = False):
+    def get_model(self, remove_above=False, remove_bellow=False):
 
-        if self.custom_pcb == True:
+        if self.custom_pcb:
             model_height = self.pcb_thickness
             down_offset = self.pcb_thickness + self.pcb_plate_separation
 
-            if remove_above == True:
+            if remove_above:
                 model_height = self.pcb_thickness + self.parameters.case_height_extra
-            elif remove_bellow == True:
+            elif remove_bellow:
                 model_height = self.pcb_thickness + self.parameters.case_height_extra
                 down_offset = down_offset + self.parameters.case_height_extra
 
-
-            pcb_model = right(self.parameters.case_wall_thickness + self.pcb_case_left_margin) ( 
-                forward(self.parameters.case_wall_thickness + self.pcb_case_bottom_margin) (
-                    down(down_offset) (
+            pcb_model = right(
+                self.parameters.case_wall_thickness + self.pcb_case_left_margin
+            )(
+                forward(
+                    self.parameters.case_wall_thickness + self.pcb_case_bottom_margin
+                )(
+                    down(down_offset)(
                         cube([self.pcb_width, self.pcb_height, model_height])
                     )
                 )
