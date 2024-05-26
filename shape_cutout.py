6fae099b5c9014f24ad894a5a2d9117e77499a98
Tim Magoun <timmagoun@gmail.com>
2024-05-26 16:20:41 -0700

    lint

--- shape_cutout.py
+++ shape_cutout.py
@@ -1,13 +1,13 @@
-from curses.textpad import rectangle
-from solid import *
-from solid.utils import *
+from solid import circle, polygon, linear_extrude, square
+
+from solid.utils import right, forward
 
 import logging
 import sys
 
 from cell import Cell
 from parameters import Parameters
-from switch_config import SwitchConfig
+
 
 class ShapeCutout(Cell):
     """
@@ -31,15 +31,16 @@ class ShapeCutout(Cell):
     """
 
     NEIGHBOR_OPOSITE_DICT = {
-        'right': 'left',
-        'left': 'right',
-        'top': 'bottom',
-        'bottom': 'top'
+        "right": "left",
+        "left": "right",
+        "top": "bottom",
+        "bottom": "top",
     }
 
-
-    def __init__(self, x, y, shape_type, shape_parameters, parameters: Parameters = Parameters()):
-        super().__init__(x, y, parameters = parameters)
+    def __init__(
+        self, x, y, shape_type, shape_parameters, parameters: Parameters = Parameters()
+    ):
+        super().__init__(x, y, parameters=parameters)
 
         self.logger = logging.getLogger().getChild(__name__)
 
@@ -49,45 +50,55 @@ class ShapeCutout(Cell):
         self.shape_parameters = shape_parameters
 
         self.shape_type_function_dict = {
-            'circle': self.circle_cutout,
-            'rectangle': self.rectangle_cutout,
-            'polygon': self.polygon_cutout
+            "circle": self.circle_cutout,
+            "rectangle": self.rectangle_cutout,
+            "polygon": self.polygon_cutout,
         }
 
         self.solid = self.get_shape_cutout()
 
-
     def __str__(self):
-        return 'Switch: ' + super().__str__()
+        return "Switch: " + super().__str__()
 
     def __repr__(self):
-        global_neighbors_json = self.neighbors_formatted(self.global_neighbors, indent=4, current_indent=10)
-        local_neighbors_json = self.neighbors_formatted(self.local_neighbors, indent=4, current_indent=10)
-        return 'Switch: ' + super().__str__() + '\nglobal neighbors: \n' + global_neighbors_json + '\local neighbors: \n' + local_neighbors_json
-
+        global_neighbors_json = self.neighbors_formatted(
+            self.global_neighbors, indent=4, current_indent=10
+        )
+        local_neighbors_json = self.neighbors_formatted(
+            self.local_neighbors, indent=4, current_indent=10
+        )
+        return (
+            "Switch: "
+            + super().__str__()
+            + "\nglobal neighbors: \n"
+            + global_neighbors_json
+            + "\nlocal neighbors: \n"
+            + local_neighbors_json
+        )
 
     def get_shape_cutout(self):
         shape = self.shape_type_function_dict[self.shape_type]()
 
-        return linear_extrude(height = 10, center = True)(shape)
-
+        return linear_extrude(height=10, center=True)(shape)
 
     def get_moved(self):
-        return right(self.x) ( forward(self.y) ( self.solid ) )
-    
+        return right(self.x)(forward(self.y)(self.solid))
+
     def circle_cutout(self):
         this_function_name = sys._getframe().f_code.co_name
         self.logger = self.logger.getChild(this_function_name)
 
-        if 'r' in self.shape_parameters.keys():
-            radius = self.shape_parameters['r']
-        elif 'd' in self.shape_parameters.keys():
-            radius = self.shape_parameters['d'] / 2
+        if "r" in self.shape_parameters.keys():
+            radius = self.shape_parameters["r"]
+        elif "d" in self.shape_parameters.keys():
+            radius = self.shape_parameters["d"] / 2
         else:
-            self.logger.error('Either a radius "r" or diameter "d" key and value must be set when createing a cutsom circle cutout')
+            self.logger.error(
+                'Either a radius "r" or diameter "d" key and value must be set when createing a cutsom circle cutout'
+            )
             exit(1)
 
-        return circle(r = radius)
+        return circle(r=radius)
 
     def rectangle_cutout(self):
         this_function_name = sys._getframe().f_code.co_name
@@ -96,23 +107,29 @@ class ShapeCutout(Cell):
         width = 0
         height = 0
         # Both width and height defined. Use both
-        if 'width' in self.shape_parameters.keys() and 'height' in self.shape_parameters.keys():
-            self.logger.warn('both height and width supplied')
-            width = self.shape_parameters['width']
-            height = self.shape_parameters['height']
-        elif 'width' in self.shape_parameters.keys():
-            self.logger.warn('only width supplied')
-            width = self.shape_parameters['width']
-            height = self.shape_parameters['width']
-        elif 'height' in self.shape_parameters.keys():
-            self.logger.warn('only height supplied')
-            width = self.shape_parameters['height']
-            height = self.shape_parameters['height']
+        if (
+            "width" in self.shape_parameters.keys()
+            and "height" in self.shape_parameters.keys()
+        ):
+            self.logger.warn("both height and width supplied")
+            width = self.shape_parameters["width"]
+            height = self.shape_parameters["height"]
+        elif "width" in self.shape_parameters.keys():
+            self.logger.warn("only width supplied")
+            width = self.shape_parameters["width"]
+            height = self.shape_parameters["width"]
+        elif "height" in self.shape_parameters.keys():
+            self.logger.warn("only height supplied")
+            width = self.shape_parameters["height"]
+            height = self.shape_parameters["height"]
         else:
-            self.logger.error('At least a "width" or "height" must be provided for a custom rectangle cutout. Specifying only 1 of those will create a squatre')
+            self.logger.error(
+                'At least a "width" or "height" must be provided for a custom rectangle cutout. Specifying only 1'
+                " of those will create a square"
+            )
             exit(1)
 
-        self.logger.warn('height: %f, width: %f', height, width)
+        self.logger.warn("height: %f, width: %f", height, width)
 
         return square([width, height])
 
@@ -121,19 +138,22 @@ class ShapeCutout(Cell):
         self.logger = self.logger.getChild(this_function_name)
 
         points = None
-        psth = None
+        path = None
 
-        if 'points' in self.shape_parameters.keys():
-            points = self.shape_parameters['points']
+        if "points" in self.shape_parameters.keys():
+            points = self.shape_parameters["points"]
         else:
-            self.logger.error('A list of points with key "points" must be provided for a custom polygon cutout')
+            self.logger.error(
+                'A list of points with key "points" must be provided for a custom polygon cutout'
+            )
             exit(1)
 
-        if 'path' in self.shape_parameters.keys():
-            path = [self.shape_parameters['path']]
+        if "path" in self.shape_parameters.keys():
+            path = [self.shape_parameters["path"]]
         else:
-            self.logger.info('No path provided for custom polygon. Will use points in order they were listed')
+            self.logger.info(
+                "No path provided for custom polygon. Will use points in order they were listed"
+            )
             path = [range(len(points))]
-        
-        return polygon(points, path)
 
+        return polygon(points, path)
